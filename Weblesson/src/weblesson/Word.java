0c10f0630203787036c4e21c075085d1d25d0913
takupoku <taku.nowet@gmail.com>
2017-09-28 13:41:03 +0900

    Update Word.java

--- Weblesson/src/weblesson/Word.java
+++ Weblesson/src/weblesson/Word.java
@@ -5,26 +5,26 @@ public class Word{
 	private String english;
 	private String japanese;
 
-	public String getEnglish(){
+	public Word(String english,String japanese){
+		this.english = english;
+		this.japanese = japanese;
+	}
+
+	@Override
+	public String toString(){
+		return "英単語 : " + english + " 日本語 " + japanese;
+	}
+
+	public String getEnglish() {
 		return english;
 	}
-	public void setEnglish(String english){
+	public void setEnglish(String english) {
 		this.english = english;
 	}
-	public String getJapanese(){
+	public String getJapanese() {
 		return japanese;
 	}
-	public void setJapanese(String japanese){
+	public void setJapanese(String japanese) {
 		this.japanese = japanese;
-	}
-
-	public Word(String english, String japanese){
-		super();
-		this.english = english;
-		this.japanese = japanese;
-	}
-
-	public String toString(){
-		return "英単語： " + this.english + "　日本語： " + this.japanese;
 	}
 }
